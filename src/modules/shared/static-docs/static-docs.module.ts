d584c9f436d1b2b55441d8812c01050fae1068ca
Souvik Sarkar <souviksarrkar362@gmail.com>
2026-05-16 22:06:33 +0530

    feat: initialize static-docs module with controller and service integration

--- src/modules/shared/static-docs/static-docs.module.ts
+++ src/modules/shared/static-docs/static-docs.module.ts
@@ -2,22 +2,15 @@ import { Module } from "@nestjs/common";
 import { FirebaseModule } from "../firebase/firebase.module";
 import { StaticDocsService } from "./application/services/static-docs.service";
 import { StaticDocsController } from "./presentation/controller/static-document.controller";
-import { DISCOVERY_PROVIDER } from "../discovery/interfaces/discovery-provider.interface";
-import { StaticDocsProvider } from "./application/providers/static-docs.provider";
 
 @Module({
     controllers: [StaticDocsController],
     imports: [FirebaseModule],
     providers: [
         StaticDocsService,
-        {
-            provide: DISCOVERY_PROVIDER,
-            useClass: StaticDocsProvider,
-        },
     ],
     exports: [
         StaticDocsService,
-        DISCOVERY_PROVIDER
     ],
 })
 export class StaticDocsModule { }
